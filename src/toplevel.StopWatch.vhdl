e96a5331142ba60b62c50b09bfa16e3ad62dc0bd
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2020-10-07 10:12:01 +0200

    Updated StopWatch toplevel to updated package pin names.

--- src/toplevel.StopWatch.vhdl
+++ src/toplevel.StopWatch.vhdl
@@ -8,20 +8,20 @@ use     work.StopWatch_pkg.all;
 
 entity toplevel is
 	generic (
-		constant CLOCK_PERIOD : time := 10 ns
+		constant CLOCK_PERIOD       : time := 10 ns
 	);
 	port (
-		Clock          : in  std_logic;
-		Reset_n        : in  std_logic;
+		NexysA7_SystemClock         : in  std_logic;
+		NexysA7_GPIO_Button_Reset_n : in  std_logic;
 		
-		Button         : in  std_logic_vector(0 downto 0);
-		Seg7_Cathode_n : out std_logic_vector(7 downto 0);
-		Seg7_Anode_n   : out std_logic_vector(7 downto 0)
+		NexysA7_GPIO_Button         : in  std_logic_vector(0 downto 0);
+		NexysA7_GPIO_Seg7_Cathode_n : out std_logic_vector(7 downto 0);
+		NexysA7_GPIO_Seg7_Anode_n   : out std_logic_vector(7 downto 0)
 	);
 end entity;
 
 
-architecture trl of toplevel is
+architecture rtl of toplevel is
 	constant STOPWATCH_CONFIGURATION : T_STOPWATCH_CONFIGURATION := (
 		0 => (Modulo => 10, Dot => '0'),
 		1 => (Modulo => 10, Dot => '0'),
@@ -48,8 +48,8 @@ architecture trl of toplevel is
 	signal Anode   : std_logic_vector(Digits'range);
 
 begin
-	-- convert low-active inputs
-	Board_Reset <= not Reset_n;
+	-- convert from low-active inputs
+	Board_Reset <= not NexysA7_GPIO_Button_Reset_n;
 
 	-- Debounce input signals
 	deb: entity work.Debouncer
@@ -58,18 +58,18 @@ begin
 			BITS          => 2
 		)
 		port map (
-			Clock     => Clock,
+			Clock     => NexysA7_SystemClock,
 			
 			Input(0)  => Board_Reset,
-			Input(1)  => Button(0),
+			Input(1)  => NexysA7_GPIO_Button(0),
 			Output(0) => Deb_Reset,
 			Output(1) => Deb_Start
 		);
 
 	Reset <= Deb_Reset;
 		
-	-- Edge detection
-	Deb_Start_d  <= Deb_Start when rising_edge(Clock);
+	-- Rising edge detection
+	Deb_Start_d  <= Deb_Start when rising_edge(NexysA7_SystemClock);
 	Deb_Start_re <= not Deb_Start_d and Deb_Start;
 	
 	-- renaming
@@ -84,7 +84,7 @@ begin
 			CONFIG        => STOPWATCH_CONFIGURATION
 		)
 		port map (
-			Clock  => Clock,
+			Clock  => NexysA7_SystemClock,
 			Reset  => Reset,
 
 			Start  => Start,
@@ -99,7 +99,7 @@ begin
 			DIGITS        => Digits'length
 		)
 		port map (
-			Clock         => Clock,
+			Clock         => NexysA7_SystemClock,
 
 			DigitValues   => Digits,
 			
@@ -107,7 +107,7 @@ begin
 			Seg7_Selects  => Anode
 		);
 
-	-- convert low-active outputs
-	Seg7_Cathode_n <= not Cathode;
-	Seg7_Anode_n   <= not ((Seg7_Anode_n'high downto Anode'length => '0') & Anode);
+	-- convert to low-active outputs
+	NexysA7_GPIO_Seg7_Cathode_n <= not Cathode;
+	NexysA7_GPIO_Seg7_Anode_n   <= not ((NexysA7_GPIO_Seg7_Anode_n'high downto Anode'length => '0') & Anode);
 end architecture;
